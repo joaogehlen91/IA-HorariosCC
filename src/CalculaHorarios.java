f6e834c7a71acf7ffe66c77a35cc9a62ab58b7f0
João Gehlen <joaogehlen91@gmail.com>
2017-04-12 17:16:39 -0300

    lendo arquivo e separando por partes

--- src/CalculaHorarios.java
+++ src/CalculaHorarios.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+
 
 
 
@@ -12,33 +11,52 @@ public class CalculaHorarios {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String fileName = "/home/joao/Trabalho1/curso.dat";
+		String fileName = "arquivos/curso.dat";
 		
 		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
 
-			String line;
-			while ((line = br.readLine()) != null) {
-				//System.out.println(line);
+			String linha;
+
+			br.readLine();
+			while((linha = br.readLine()).length() > 0){
+				addProfessor(linha);
+			}
+
+			br.readLine();
+			while((linha = br.readLine()).length() > 0){
+				addSemestre(linha);
+			}
+
+			br.readLine();
+			while((linha = br.readLine()) != null){
+				addDisciplina(linha);
 			}
 			
 			
-			Horario h = new Horario();
-			
+/*			Horario h = new Horario();
 			Disciplina ds = new Disciplina("IA");
-			
 			h.setAula(8, ds);
-			
-			
-			
 			h.printHorarioss();
-			
-			
-			
+*/			
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
+	}
+	
+		
+	
+	private static void addProfessor(String linha){
+		System.out.println(linha);
+	}
+	
+	private static void addSemestre(String linha){
+		System.out.println(linha);
+	}
+	
+	private static void addDisciplina(String linha){
+		System.out.println(linha);
 	}
 
 }
